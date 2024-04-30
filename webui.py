20ddb83d0d471a7871cf4a06819043fce0382ebe
zhangjl19 <zhangjl19@spdb.com.cn>
2024-04-30 14:53:31 +0800

    add codes

--- webui.py
+++ webui.py
@@ -15,8 +15,8 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-del os.environ['http_proxy']
-del os.environ['https_proxy']
+# del os.environ['http_proxy']
+# del os.environ['https_proxy']
 
 import torch
 import utils
